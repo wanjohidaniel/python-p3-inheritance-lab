9e1dad2944865099f62e44d3f5e1d6f91b80fd06
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-05-27 20:27:39 +0300

    Object Inheritance Lab Part One

--- lib/testing/teacher_test.py
+++ lib/testing/teacher_test.py
@@ -1,26 +1,27 @@
-#!/usr/bin/env python3
+import unittest
+from lib.teacher import Teacher
 
-from teacher import Teacher
-from user import User
-
-my_teacher = Teacher("My", "Teacher")
-
-class TestTeacher:
-    '''Class "Teacher" in teacher.py'''
+class TestTeacher(unittest.TestCase):
+    def setUp(self):
+        self.my_teacher = Teacher("My", "Teacher")
 
     def test_is_subclass(self):
         '''is a subclass of "User".'''
-        assert(User in Teacher.__bases__)
+        from lib.user import User
+        self.assertTrue(issubclass(Teacher, User))
 
     def test_initializes_with_names(self):
         '''initializes with first and last name.'''
-        assert((my_teacher.first_name, my_teacher.last_name) == ("My", "Teacher"))
+        self.assertEqual((self.my_teacher.first_name, self.my_teacher.last_name), ("My", "Teacher"))
 
     def test_has_attribute_knowledge(self):
         '''has an attribute called "knowledge", a list with len > 0.'''
-        assert(isinstance(my_teacher.knowledge, list) and len(my_teacher.knowledge) > 0)
+        self.assertIsInstance(self.my_teacher.knowledge, list)
+        self.assertGreater(len(self.my_teacher.knowledge), 0)
 
     def test_can_teach(self):
         '''teaches from list of knowledge.'''
-        my_teacher = Teacher("My", "Teacher")
-        assert(my_teacher.teach() in my_teacher.knowledge)
+        self.assertIn(self.my_teacher.teach(), self.my_teacher.knowledge)
+
+if __name__ == '__main__':
+    unittest.main()
